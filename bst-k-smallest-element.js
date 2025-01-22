9f65a4a3ffffec78e02517f8f722bbfacaa29cfd
Vikrant negi <vikrant.negi74@gmail.com>
2025-01-23 04:47:45 +0530

    feat: kth smallest in bst

---
+++ bst-k-smallest-element.js
@@ -0,0 +1,27 @@
+// Given the root node of a binary search tree (BST) and an integer k,
+//write a function to find and return the k-th smallest value in the BST. The smallest value in the tree is 1.
+// The binary tree is represented by a collection of TreeNodes, where each node has optional left and right child nodes,
+// which are also TreeNodes.
+
+// Approach 1: Iterate through the tree and find the k-th smallest element
+// O(H+k)
+
+const kthSmallestElementInABst = (root, k) => {
+  let result;
+
+  const iterateRoot = (root) => {
+    if (!root) return;
+
+    iterateRoot(root.left);
+
+    if (root.val) {
+      k--;
+      if (k === 0) result = root.val;
+    }
+
+    iterateRoot(root.right);
+  };
+
+  iterateRoot(root);
+  return result;
+};
